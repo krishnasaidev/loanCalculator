5b3210f67e5fb00b335135f4ddca520ba3bde26b
krishna sai <krishnasaiv.dev@gmail.com>
2025-02-26 17:05:52 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'loanCalculator';
+  maximumFunding = 17484500;
+  loanAmount = 14500000;
+  minLoan = 1000000;
+  maxLoan = 17480000;
+
+  loanPeriod = 12;
+  minPeriod = 6;
+  maxPeriod = 18;
+
+  calculateMonthlyPayment(): number {
+    return Math.round(this.loanAmount / this.loanPeriod);
+  }
+
+  applyLoan() {
+    console.log(this.loanAmount)
+  }
+
 }
